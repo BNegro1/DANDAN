8f74c6da79bb5901e7c1517b5faed2168fdb1c84
bNegro <72948318+BNegro1@users.noreply.github.com>
2023-12-12 22:13:29 -0300

    Cambios varios realizados.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  template: '<div class="chart-container"><canvas id="myChart" width="400" height="400"></canvas></div>',
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
 
